13b09f0956bbf411ec8cc4af61d2c79555ea6abd
Trần Văn Giáp <101784645+ZipTGhz@users.noreply.github.com>
2024-10-17 17:56:42 +0700

    cập nhật: xong bài 9

---
+++ Areas/Admin/Controllers/HomeAdminController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Web_QuanLySieuThiNho.Models;
+using X.PagedList;
+
+namespace Web_QuanLySieuThiNho.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin")]
+    [Route("Admin/Home")]
+    public class HomeAdminController : Controller
+    {
+        private QlsieuThiNhoContext _db = new QlsieuThiNhoContext();
+        [Route("")]
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("DanhMucSanPham")]
+        public IActionResult DanhMucSanPham(int pageNumber = 1)
+        {
+            int pageSize = 6;
+            var products = _db.TSanPhams.AsNoTracking().OrderBy(x => x.DonGiaBan);
+            PagedList<TSanPham> pagedList = new PagedList<TSanPham>(products, pageNumber, pageSize);
+            return View(pagedList);
+        }
+
+    }
+}
