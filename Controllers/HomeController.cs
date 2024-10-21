736aa90300863e82edda6ddb2c24255e1a0d02f0
Trần Văn Giáp <101784645+ZipTGhz@users.noreply.github.com>
2024-10-22 00:13:17 +0700

    cập nhật: thêm tính năng sắp xếp, sử dụng bootstrap cho phân trang.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,14 +1,15 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 using Web_QuanLySieuThiNho.Models;
 using X.PagedList;
+using X.PagedList.Extensions;
 
 namespace Web_QuanLySieuThiNho.Controllers
 {
     public class HomeController : Controller
     {
-        private QlsieuThiNhoContext db = new QlsieuThiNhoContext();
+        private QlsieuThiNhoContext _db = new QlsieuThiNhoContext();
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -19,30 +20,36 @@ namespace Web_QuanLySieuThiNho.Controllers
         public IActionResult Index(int pageNumber = 1)
         {
             HttpContext.Session.SetString("username", "user");
-            int pageSize = 12;
-            var products = db.TSanPhams.AsNoTracking().OrderBy(x => x.TenSanPham);
+            int pageSize = 9;
+            var products = _db.TSanPhams.AsNoTracking().OrderBy(x => x.TenSanPham);
 
             PagedList<TSanPham> pagedList = new PagedList<TSanPham>(products, pageNumber, pageSize);
 
             return View(pagedList);
         }
-        public IActionResult Category(string categoryId, int pageNumber = 1)
+        public IActionResult Category(string categoryId, string sortOrder = "default", int pageNumber = 1)
         {
-            int pageSize = 12;
+            int pageSize = 9;
 
-            var filteredProducts = db.TSanPhams.AsNoTracking()
-                .Where(x => x.MaLoaiHang == categoryId)
-                .OrderBy(x => x.TenSanPham);
+            var filteredProducts = _db.TSanPhams.AsNoTracking()
+                .Where(x => x.MaLoaiHang == categoryId);
 
-            PagedList<TSanPham> pagedList = new PagedList<TSanPham>(filteredProducts, pageNumber, pageSize);
+            filteredProducts = sortOrder switch
+            {
+                "asc" => filteredProducts.OrderBy(x => x.DonGiaBan),
+                "desc" => filteredProducts.OrderByDescending(x => x.DonGiaBan),
+                _ => filteredProducts.OrderBy(x => x.MaSanPham),
+            };
+
+            var pagedList = new PagedList<TSanPham>(filteredProducts, pageNumber, pageSize);
 
             ViewBag.maLoaiHang = categoryId;
+            ViewBag.SortOrder = sortOrder;
             return View(pagedList);
         }
-
         public IActionResult Detail(string productId)
         {
-            var product = db.TSanPhams.SingleOrDefault(x => x.MaSanPham == productId);
+            var product = _db.TSanPhams.SingleOrDefault(x => x.MaSanPham == productId);
             return View(product);
         }
         public IActionResult Cart()
