43a96add0d83f043f8ecc3f84d0daafc35abaa21
Trần Văn Giáp <101784645+ZipTGhz@users.noreply.github.com>
2024-10-30 16:05:20 +0700

    cập nhật model csdl

--- Models/TTaiKhoan.cs
+++ Models/TTaiKhoan.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 
 namespace Web_QuanLySieuThiNho.Models;
 
 public partial class TTaiKhoan
 {
-    [Required]
-
     public string TenDangNhap { get; set; } = null!;
 
     public string? MatKhau { get; set; }
